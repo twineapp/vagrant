d0fff4df93de80a5346c23ee8903dcca0a702c71
jdu <jduren@gmail.com>
2013-06-04 18:09:21 +0100

    Changes to config-loca.js template

--- puppet/templates/config-local.js
+++ puppet/templates/config-local.js
@@ -2,6 +2,7 @@ define('config-local',function(){
 	// Local Configuration overrides
 	var c = {
 		hostname: 'http://' + window.location.host,
+		etlPath: 'http://192.168.50.4/etl/',
 		logoPath: 'assets/images/logos/siv-logo-demo.png',
 		headerClass: 'siv-header-demo',
 		appTitle: 'twine',
