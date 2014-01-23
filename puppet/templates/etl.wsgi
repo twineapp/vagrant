14802c81bedc6208af9590f268aad95fbfe650fa
JDU <jeff@jdu-it.co.uk>
2014-01-23 17:16:04 +0000

    Updated vagrant configuration for the refactor of the python api

--- puppet/templates/etl.wsgi
+++
@@ -1,5 +0,0 @@
-import sys
-sys.path.insert(0, '/var/www/siv-v3/api-data/ETL')
-
-from etl import create_app
-application = create_app()
--- src/scripts/etl-setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo "change directory to /var/www/flaskapps/etl/"
-cd /var/www/siv-v3/api-data
-
-python setup.py install
-
