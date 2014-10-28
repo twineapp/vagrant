7839880d280a592549995a387862e9a009ad1b7b
Mark Yarmoshuk <mark.yarmoshuk@gmail.com>
2014-10-28 18:38:12 -0400

    version change to 3.25

--- src/scripts/postgres.build.sh
+++ src/scripts/postgres.build.sh
@@ -3,17 +3,24 @@
 # build / patch files are placed in /var/www/vagrant/src/postgres/
 # ensure that only patch files for a single instance are located in the directory
 
+# !! password suffix is specified in line 54
+# !! schema build queries must be edited to add in instance specific variable
+
 # to run manually
 #	vagrant ssh
 #	. /var/www/vagrant/src/scripts/postgres.build.sh
 
+# add  > /dev/null 2>&1 to end of line to supress output
+
 instance=twine
+baseurl=siv-v3
+usersuffix=_unhcr
 
 echo "Creating twine ($instance instance) database"
 #terminate current connections
-sudo -u postgres psql -q -c "SELECT pg_terminate_backend(pid) FROM pg_stat_get_activity(NULL::integer) WHERE datid=(SELECT oid from pg_database where datname = '$instance');" > /dev/null 2>&1
+sudo -u postgres psql -q -c "SELECT pg_terminate_backend(pid) FROM pg_stat_get_activity(NULL::integer) WHERE datid=(SELECT oid from pg_database where datname = '$instance');"
 #drop database
-sudo -u postgres psql -q -c "DROP DATABASE IF EXISTS $instance;" > /dev/null 2>&1
+sudo -u postgres psql -q -c "DROP DATABASE IF EXISTS $instance;"
 # create database
 sudo -u postgres psql -q -c "CREATE DATABASE $instance WITH TEMPLATE=template0 OWNER=admin ENCODING='UTF8' LC_COLLATE='en_US.utf8' LC_CTYPE='en_US.utf8';"
 
@@ -24,34 +31,41 @@ echo "    create extensions"
 sudo -u postgres psql -q $instance < pre.extensions.sql
 
 echo "    creating siv_app schema and tables"
-sudo -u postgres psql -q $instance < siv_app.sql > /dev/null 2>&1
+sudo -u postgres psql -q $instance -v v_instance="$instance" < siv_app.sql
 echo "    populating siv_app tables"
-sudo -u postgres psql -q $instance < siv_app.data.sql > /dev/null 2>&1
+sudo -u postgres psql -q $instance < siv_app.data.sql
 
 echo "    creating siv_data schema and tables"
-sudo -u postgres psql -q $instance < siv_data.sql > /dev/null 2>&1
+sudo -u postgres psql -q $instance -v v_instance="$instance" < siv_data.sql
 echo "    populating siv_data tables"
-sudo -u postgres psql -q $instance < siv_data.data.sql > /dev/null 2>&1
+sudo -u postgres psql -q $instance < siv_data.data.sql
 
 echo "    creating siv_users schema and tables"
-sudo -u postgres psql -q $instance < siv_users.sql > /dev/null 2>&1
+sudo -u postgres psql -q $instance -v v_instance="$instance" < siv_users.sql
 echo "    populating siv_users tables"
-sudo -u postgres psql -q $instance < siv_users.data.sql > /dev/null 2>&1
+sudo -u postgres psql -q $instance < siv_users.data.sql
+
+echo "    creating siv_staging schema and tables"
+sudo -u postgres psql -q $instance -v v_instance="$instance" < siv_staging.sql
 
 echo "    adjusting auto increments"
-sudo -u postgres psql -q $instance < post.autoincrement.sql > /dev/null 2>&1
+sudo -u postgres psql -q $instance < post.autoincrement.sql
 
 echo "    creating database users"
-sudo -u postgres psql -q $instance < $instance.dbusers.sql > /dev/null 2>&1
+sudo -u postgres psql -q $instance -v v_instance="$instance"  -v v_usersuffix="$usersuffix" -v v_pwd1="'unhcr'" -v v_pwd2="'unhcr'" < siv.dbusers.sql
 
 echo "    applying SQL patches"
 for f in [0-9][0-9]*.sql; do
 	echo "        on file: $f"
-	sudo -u postgres psql -q $instance < "$f" > /dev/null 2>&1
+#	if $instance in $f; then
+		sudo -u postgres psql -q $instance < "$f"
+#	else
+#		echo "        - skipping"
+#	fi
 done
 
 echo "    clearing python bytecode"
-find /var/www/siv-v3/api-data -name "*.pyc" -exec rm -rf {} \;
+find /var/www/$baseurl/api-data -name "*.pyc" -exec rm -rf {} \;
 
 echo "    restating apache"
 sudo service apache2 restart
