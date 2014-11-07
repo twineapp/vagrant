b8692b9674f207fe4d85e097ed760073c1780aa1
jdu <jeff@jdu-it.co.uk>
2014-11-07 14:21:41 +0000

    Changes to use puppet instead of sh script for setting up language on server

--- puppet/manifests/twine.pp
+++ puppet/manifests/twine.pp
@@ -10,6 +10,28 @@ exec
         command => '/usr/bin/apt-get update'
 }
 
+# This needs to be done before anything else in order to ensure
+# that subsequent package installs are configured for language correctly
+exec { "dpkg-locales":
+    command => "sudo dpkg-reconfigure locales"
+}
+
+# puppet does a check to see if the first item in a command maps to an actual executable, if it doesn't
+# it will throw an error, for is a language construct and not a binary so it will fail unless there is something
+# before it which evalutes to an executable, we get aroudn this by calling cat "" (a useless function) so that the rest
+# of the command gets passed to the shell and executed
+exec { 'locale-gen':
+    command => 'sudo locale-gen fr_FR.UTF-8; sudo locale-gen en_US.UTF-8; sudo locale-gen de_DE.UTF-8; sudo locale-gen pt_BR.ISO-8859-1; sudo locale-gen es_ES.UTF-8;',
+    require => [Exec['dpkg-locales']]
+}
+
+
+exec { 'install-gettext':
+    command => 'sudo apt-get install -y gettext',
+    require => [Exec['locale-gen']]
+}
+
+
 include bootstrap
 include other
 include apache
@@ -30,5 +52,5 @@ exec
 {
     'service apache2 reload':
         command => 'service apache2 reload',
-        require => [ Exec["add-oauth-extension"], Exec["add-mongo-extension"] ]
+        require => [ Exec["add-oauth-extension"], Exec["add-mongo-extension"], Exec['install-gettext'] ]
 }
--- puppet/modules/twine/manifests/init.pp
+++ puppet/modules/twine/manifests/init.pp
@@ -1,81 +1,68 @@
-class twine 
-{      
-    
-    file 
-    { 
-        "/etc/apache2/sites-available/default":
-            ensure  => present,
-            source  => "/var/www/vagrant/puppet/templates/vhost",
-            require => [ Package["apache2"], File["/etc/apache2/mods-enabled/wsgi.load"] ]
-    }
+class twine
+{
 
-    exec
-    {
-        "mongorestore-his":
-            command => 'mongorestore --drop -d siv /var/www/vagrant/src/mongo/his.bson',
-            timeout => 3600,
-            require => Package["mongodb-10gen"],
-            onlyif  => 'test -f /var/www/vagrant/src/mongo/his.bson',
-    }
-    
-    exec
-    {
-        "mongorestore-de-urban":
-            command => 'mongorestore --drop -d siv /var/www/vagrant/src/mongo/de_urban.bson',
-            timeout => 3600,
-            require => Package["mongodb-10gen"],
-            onlyif  => 'test -f /var/www/vagrant/src/mongo/de_urban.bson',
-    }
-    
-    /*
-    exec
-    {
-        "mongorestore-de-foodaid":
-            command => 'mongorestore --drop -d siv /var/www/vagrant/src/mongo/de_foodaid.bson',
-            timeout => 3600,
-            require => Package["mongodb-10gen"],
-            onlyif  => 'test -f /var/www/vagrant/src/mongo/de_foodaid.bson',
-    }
-    */
+  file { "/etc/apache2/sites-available/default":
+      ensure  => present,
+      source  => "/var/www/vagrant/puppet/templates/vhost",
+      require => [ Package["apache2"], File["/etc/apache2/mods-enabled/wsgi.load"] ]
+  }
 
-    exec { "setup-twine-tools":
-        command => 'sudo ln -sf /var/www/vagrant/src/scripts/twine-tools /usr/bin/twine-tools'
-    }
+  exec { "mongorestore-his":
+      command => 'mongorestore --drop -d siv /var/www/vagrant/src/mongo/his.bson',
+      timeout => 3600,
+      require => Package["mongodb-10gen"],
+      onlyif  => 'test -f /var/www/vagrant/src/mongo/his.bson',
+  }
 
-    file 
-    { 
-        "/var/www/siv-v3/siv.ini":
-            ensure  => present,
-            source  => "/var/www/vagrant/puppet/templates/siv.ini",
-            require => Package['apache2'],
-    }
+  exec { "mongorestore-de-urban":
+      command => 'mongorestore --drop -d siv /var/www/vagrant/src/mongo/de_urban.bson',
+      timeout => 3600,
+      require => Package["mongodb-10gen"],
+      onlyif  => 'test -f /var/www/vagrant/src/mongo/de_urban.bson',
+  }
 
-    file 
-    { 
-        "/var/www/siv-v3/app/config.json":
-            ensure  => present,
-            source  => "/var/www/vagrant/puppet/templates/config.json",
-            require => Package['apache2'],
-    }
+/*
+exec
+{
+    "mongorestore-de-foodaid":
+        command => 'mongorestore --drop -d siv /var/www/vagrant/src/mongo/de_foodaid.bson',
+        timeout => 3600,
+        require => Package["mongodb-10gen"],
+        onlyif  => 'test -f /var/www/vagrant/src/mongo/de_foodaid.bson',
+}
+*/
 
-    file 
-    {
-	"/var/www/siv-v3/filestore":
-	    ensure => "directory",
-	    owner  => "root",
-	    group  => "root",
-	    mode   => 777,
-    }
 
-    file
-    {
-	"/home/TwineHeadlessBrowser":
-	    ensure => present,
-	    source => "/var/www/vagrant/puppet/templates/TwineHeadlessBrowser",
-	    group   => "root",
-	    owner   => "root",
-	    mode    => 777	
-    }
+  exec { "setup-twine-tools":
+    command => 'sudo ln -sf /var/www/vagrant/src/scripts/twine-tools /usr/bin/twine-tools'
+  }
+
+  file { "/var/www/siv-v3/siv.ini":
+      ensure  => present,
+      source  => "/var/www/vagrant/puppet/templates/siv.ini",
+      require => Package['apache2'],
+  }
+
+  file { "/var/www/siv-v3/app/config.json":
+      ensure  => present,
+      source  => "/var/www/vagrant/puppet/templates/config.json",
+      require => Package['apache2'],
+  }
+
+  file { "/var/www/siv-v3/filestore":
+      ensure => "directory",
+      owner  => "root",
+      group  => "root",
+      mode   => 777,
+  }
+
+  file { "/home/TwineHeadlessBrowser":
+      ensure  => present,
+      source  => "/var/www/vagrant/puppet/templates/TwineHeadlessBrowser",
+      group   => "root",
+      owner   => "root",
+      mode    => 777
+  }
 
 
 }
